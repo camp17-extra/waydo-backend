96af4ec639420f0a89d7136d5afdc4af5f207bd4
KoichiOzaki <goingmyway19851224@gmail.com>
2018-02-24 18:31:26 +0900

    add: posts一覧api

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::PostsController < ApplicationController
   def index
-    render json: { "message": , "status": status_code }, status: 200
+    @posts = Post.all.order(created_at: :desc).limit(10)
+    render json: @posts, adapter: :json, status: 200
   end
 end
